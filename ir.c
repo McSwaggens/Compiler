a96802b6ccc84f41da283daca1bc0f25c2245dd3
McSwaggens <McSwaggens@protonmail.com>
2024-12-21 10:48:24 +1100

    Initialized integer value constants.

--- ir.c
+++ ir.c
@@ -2,10 +2,10 @@
 #include "ast.h"
 #include "alloc.h"
 
-static Value* value_pool = null;
+static Value* value_stack = null;
 static const u64 VALUE_POOL_LENGTH = 1 << 30;
-static Value* value_pool_head = null;
-static Value* value_pool_end  = null;
+static Value* value_stack_head = null;
+static Value* value_stack_end  = null;
 
 typedef struct VcHashTable_Entry {
 	u64 const_value;
@@ -20,22 +20,29 @@ typedef struct VcHashTable_EntryBlock {
 VcHashTable_EntryBlock constant_hashtable[4096];
 
 static void ir_init(void) {
-	value_pool = alloc(sizeof(Value) * VALUE_POOL_LENGTH);
-	value_pool_head = value_pool;
-	value_pool_end = value_pool + VALUE_POOL_LENGTH;
+	value_stack = alloc(sizeof(Value) * VALUE_POOL_LENGTH);
+	value_stack_head = value_stack;
+	value_stack_end = value_stack + VALUE_POOL_LENGTH;
+
+	for (int i = 0; i < 256; i++) {
+		value_stack[i] = (Value){
+			.flags   = VALUE_FLAG_CONSTANT,
+			.integer = -128 + i,
+		};
+	}
 
 	zero(constant_hashtable, sizeof(constant_hashtable));
 }
 
 static Value* alloc_value(void) {
-	return value_pool_head++;
+	return value_stack_head++;
 }
 
 static RelationSet expand_relation_set(RelationSet* old);
 
 static Value* ir_const_int(u64 n) {
 	if (n + 128 < 256)
-		return value_pool + (n + 128);
+		return value_stack + (n + 128);
 
 	VcHashTable_EntryBlock* block = &constant_hashtable[hash64(n) & 4095];
 
